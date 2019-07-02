f86bb706c49ec5bd5503837e73597d9c17dc84a9
Jorge Contreras <jorge.contreras789@gmail.com>
2019-07-02 14:58:46 -0400

    Updates

---
+++ precache-manifest.2010c63683eea183ef8422e4b9eace23.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4626a4ee869e528b804fcb72324a3d21",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "51b61cf950f7e3b37d83",
+    "url": "/monsters-rolodex/static/css/main.b4c3aa5b.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "51b61cf950f7e3b37d83",
+    "url": "/monsters-rolodex/static/js/main.e2c6dda8.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
